dacb85c51032393e3d0b9ac968d74f66deb497f7
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-15 17:11:24 +0200

    installed lenis + setup the placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+const array = new Array(5).fill(0);
+
+const Placeholder = () => {
+  return (
+    <section className="h-screen flex justify-center items-center">
+      <div className="h-[20vw] w-[20vw] bg-blue-300"></div>
+    </section>
+  );
+};
 
 function App() {
-
   return (
-    <main className="bg-red-400">
-    Yo
+    <main className="min-h-screen bg-slate-100">
+      {array.map((idx) => (
+        <Placeholder key={idx} />
+      ))}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
