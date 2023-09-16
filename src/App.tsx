66ec4d42c0964d310e9b168930302fb7f851675f
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-16 17:58:29 +0200

    made a horizontal slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import { useLenis } from "./contexts/LenisContext";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const array = new Array(5).fill(0);
+const array = new Array(3).fill(0);
+const sliderArray = ["blue", "green", "red"];
 
 const Placeholder = () => {
   return (
@@ -10,11 +13,71 @@ const Placeholder = () => {
   );
 };
 
+const PinnedPlaceholder = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+  }, []);
+
+  useEffect(() => {
+    if (!ref.current || !sliderRef.current) return;
+
+    // gsap.to(ref.current, {
+    //   scrollTrigger: {
+    //     trigger: ref.current,
+    //     start: "top top",
+    //     end: "bottom top",
+    //     pin: true,
+    //     pinSpacing: false,
+    //     scrub: true,
+    //   },
+    // });
+
+    gsap.to(sliderRef.current, {
+      xPercent: -100 * (sliderArray.length - 1),
+      scrollTrigger: {
+        trigger: ref.current,
+        start: "top top",
+        markers: true,
+        end: "bottom top",
+        scrub: 0.9,
+        pin: true,
+        pinSpacing: false,
+        snap: {
+          // or "labels" or function or Array
+          duration: 0.5,
+          directional: true,
+          ease: "power3",
+          // other callbacks: onStart, onInterrupt
+        },
+      },
+    });
+  }, [ref.current, sliderRef.current]);
+
+  return (
+    <section ref={ref} className="h-[300vh]">
+      <div ref={sliderRef} className="flex">
+        {sliderArray.map((color, idx) => (
+          <div
+            className="h-screen w-screen flex-none border border-red-400"
+            style={{ backgroundColor: color }}
+            key={idx}
+          ></div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
 function App() {
-  const lenis = useLenis();
-  console.log(lenis);
   return (
     <main className="min-h-screen bg-slate-100">
+      {array.map((_, idx) => (
+        <Placeholder key={idx} />
+      ))}
+      <PinnedPlaceholder />
       {array.map((_, idx) => (
         <Placeholder key={idx} />
       ))}
