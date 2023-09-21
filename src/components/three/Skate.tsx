d8e59d071c6e478ba06f5bb90bf2a7ccd944f540
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-21 16:20:10 +0200

    positionned the skate

--- src/components/three/Skate.tsx
+++ src/components/three/Skate.tsx
@@ -1,18 +1,41 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useWindowSize } from "../../hooks";
 import { useFrame } from "@react-three/fiber";
+import { useControls } from "leva";
 
 const SKATE_SRC = "./assets/exportSk.gltf";
 
 export const Skate = () => {
+  // set up
   const skateRef = useRef();
   const { width } = useWindowSize();
   const skateScale = width ? width * 0.1 : 0;
   const model = useGLTF(SKATE_SRC);
 
+  const controls = useControls("Position and Rotation", {
+    rotation: {
+      value: [-1.5, 0.35, -0.55],
+      min: -180,
+      max: 180,
+      step: 0.01,
+    },
+    position: {
+      value: [-24, 0, 0],
+      min: -180,
+      max: 180,
+    },
+  });
+
+  useEffect(() => {}, []);
+  // // controls
+  // const controls = useControls("Position and Rotation", {
+  //   rotation,
+  // });
+
   useFrame(() => {
-    console.log(skateRef.current.position);
+    skateRef.current?.rotation.set(...controls.rotation);
+    skateRef.current.position.set(...controls.position);
   });
 
   return (
