5c54c718b1428db7d24f9665a25dffaddd3755f2
Serotoninene <pujol.alexandre@hotmail.fr>
2023-09-27 17:38:31 +0200

    fixed a typescript issue by declaring the glsl file is readable

--- src/components/three/Fluids/index.tsx
+++ src/components/three/Fluids/index.tsx
@@ -1,11 +1,42 @@
-import vertex from "./shaders/vertex.glsl";
+import { useRef, useMemo } from "react";
+
+import { useFrame } from "@react-three/fiber";
+
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
 
 export const Fluids = () => {
-  console.log(vertex);
+  const shaderRef = useRef<any>(null);
+
+  const uniforms = useMemo(
+    () => ({
+      uTime: { value: 0 },
+      uMouse: { value: { x: 0.0, y: 0.0 } },
+      uRadius: { value: 0.07 },
+      uIntensity: { value: 0.02 },
+      uIntro: { value: 0 },
+    }),
+    []
+  );
+
+  useFrame((props, delta) => {
+    const { mouse } = props;
+
+    if (!shaderRef.current) return;
+    shaderRef.current.uniforms.uTime.value += delta;
+    shaderRef.current.uniforms.uMouse.value = { x: mouse.x, y: mouse.y };
+  });
+
   return (
     <mesh>
       <boxGeometry args={[500, 500, 1]} />
-      <meshBasicMaterial color={0x0000ff} />
+      {/* <meshBasicMaterial color="red" /> */}
+      <shaderMaterial
+        ref={shaderRef}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+        uniforms={uniforms}
+      />
     </mesh>
   );
 };
