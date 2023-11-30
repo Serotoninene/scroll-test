bdb9f009eb1b17ebac23bc1880130e941155a07c
Serotoninene <pujol.alexandre@hotmail.fr>
2023-11-30 17:45:04 +0100

    setup the shaders

--- src/components/three/Particles/index.tsx
+++ src/components/three/Particles/index.tsx
@@ -1,10 +1,16 @@
 import { useMemo, useRef } from "react";
 import * as THREE from "three";
 
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+import { useFrame } from "@react-three/fiber";
+
 const PARTICLES: number = 10000;
 
 export default function Particles() {
+  const shaderRef = useRef<THREE.ShaderMaterial>(null);
   const particlesRef = useRef<THREE.Points>(null);
+
   const particlesPosition = useMemo(() => {
     const particlesPosition = new Float32Array(PARTICLES * 3);
     const distance = 250;
@@ -20,6 +26,19 @@ export default function Particles() {
     return particlesPosition;
   }, [PARTICLES]);
 
+  const uniforms = useMemo(
+    () => ({
+      uTime: { value: 0 },
+    }),
+    []
+  );
+
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    if (!shaderRef.current) return;
+    shaderRef.current.uniforms.uTime.value = elapsedTime;
+  });
+
   return (
     <points ref={particlesRef}>
       <bufferGeometry attach="geometry">
@@ -31,7 +50,13 @@ export default function Particles() {
           normalized={false}
         />
       </bufferGeometry>
-      <pointsMaterial attach="material" size={5} color="red" />
+      <shaderMaterial
+        ref={shaderRef}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+        uniforms={uniforms}
+      />
+      {/* <pointsMaterial attach="material" size={5} color="red" /> */}
     </points>
   );
 
---
+++ src/components/three/Particles/shaders/vertex.glsl
@@ -0,0 +1,23 @@
+uniform float uTime;
+
+mat3 rotation3dY(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+  return mat3(
+    c, 0.0, -s,
+    0.0, 1.0, 0.0,
+    s, 0.0, c
+  );
+}
+
+
+void main() {
+  vec3 particlePosition = position * rotation3dY(uTime * 0.2);
+
+  vec4 modelPosition = modelMatrix * vec4(particlePosition, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+  gl_PointSize = 5.0;
+}
